cab82ad550ec61ca86815a09643e39b48c97cf9c
Dragos-Ioan Anghel <craftingchest@icloud.com>
2026-04-23 14:10:50 +0200

    Added devices and started with the devicelist

--- src/main/java/at/spengergasse/spring_thymeleaf/repositories/DeviceRepository.java
+++ src/main/java/at/spengergasse/spring_thymeleaf/repositories/DeviceRepository.java
@@ -1,5 +1,6 @@
-package at.spengergasse.spring_thymeleaf.entities;
+package at.spengergasse.spring_thymeleaf.repositories;
 
+import at.spengergasse.spring_thymeleaf.entities.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/at/spengergasse/spring_thymeleaf/repositories/ReservationRepository.java
+++ src/main/java/at/spengergasse/spring_thymeleaf/repositories/ReservationRepository.java
@@ -1,5 +1,6 @@
-package at.spengergasse.spring_thymeleaf.entities;
+package at.spengergasse.spring_thymeleaf.repositories;
 
+import at.spengergasse.spring_thymeleaf.entities.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
